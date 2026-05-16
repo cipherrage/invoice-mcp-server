4b6a5da7a74c314bd434ea6f9012676d298d3dd9
LASmall <leon.small@thecrane.com>
2026-05-16 13:42:09 -0400

    Updated Server logic

--- inv_server.py
+++ inv_server.py
@@ -1,12 +1,29 @@
 from fastmcp import FastMCP
+from dotenv import load_dotenv
 from starlette.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
 
+# Load Environment variables
+load_dotenv()
+
+# Create new MCP App
 mcp = FastMCP(name="Invoice MCP Server")
 
 @mcp.tool
 def greet(name: str) -> str:
     """Greet a person by name."""
     return f"Hello, {name}!"
+
+@mcp.tool
+def get_my_notes() -> str:
+    """Get all notes for a user"""
+    return "No notes"
+
+@mcp.tool
+def add_note(content:str) -> str:
+    """Add a note for a user"""
+    return f"added note: {content}"
 
 @mcp.custom_route("/health", methods=["GET"])
 async def health_check(_request):
@@ -25,4 +42,17 @@ async def health_check(_request):
 
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="127.0.0.1", port=8000)
+    mcp.run(
+        transport="http", 
+        host="127.0.0.1", 
+        port=8000,
+        middleware=[
+            Middleware(
+                CORSMiddleware,
+                allow_origins=["*"],
+                allow_credentials=True,
+                allow_methods=["*"],
+                allow_headers=["*"],
+            )
+        ]
+    )
